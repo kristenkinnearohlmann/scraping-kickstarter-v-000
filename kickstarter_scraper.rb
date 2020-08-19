61f497932bb62253cf8d5d7a216660025a1d13b7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-19 19:25:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,8 +12,14 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 
   binding.pry
 end
